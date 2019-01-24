680a1f458606c875a62fb832d8a5d68a320e11bc
garipov_a <garipov_a@ufanet.ru>
2019-01-24 17:42:06 +0500

    [WIP][R]Taking topic name from properties, don't need in fun for topic name

--- BooleanGroupWebService/src/main/kotlin/ru/hei/web/service/BooleanGroupKafkaMessage.kt
+++ BooleanGroupWebService/src/main/kotlin/ru/hei/web/service/BooleanGroupKafkaMessage.kt
@@ -1,25 +1,19 @@
 package ru.hei.web.service
 
-import BooleanGroupData
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class BooleanGroupKafkaMessage {
-    var topicName: String = ""
+    @Value("\${matrix.group.producer.topic}")
+    private lateinit var topicName: String
     private val mapper = jacksonObjectMapper()
 
     @Autowired
     lateinit var kafkaTemplate: KafkaTemplate<String, String>
 
-    fun withTopic(topicName: String): BooleanGroupKafkaMessage {
-        this.topicName = topicName
-        return this
-    }
-
-    fun send(data: BooleanGroupData) {
-        kafkaTemplate.send(topicName, mapper.writeValueAsString(data))
-    }
+    fun send(data: BooleanGroupData) = kafkaTemplate.send(topicName, mapper.writeValueAsString(data))
 }
